711820401799516c1e0f22f4083dcfc6a13ef4b3
Ewerton Cordeiro Miranda <sonardevplus@gmail.com>
2022-07-19 09:03:27 -0300

    adicao das dependencias

---
+++ src/main/java/com/miranda/sqsfila/SenderQueueApplication.java
@@ -0,0 +1,13 @@
+package com.miranda.sqsfila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenderQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenderQueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miranda/sqsfila/SenderQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miranda.sqsfila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenderQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
